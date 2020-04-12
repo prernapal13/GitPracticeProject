93e4dca1f9b2bebd2488368616f186270c855cb0
Anuj Kumar <cdac.anuj@gmail.com>
2020-04-12 15:33:52 +0530

    committing new files

---
+++ src/main/Class1.java
@@ -0,0 +1,7 @@
+public class Class1 {
+
+    public void method1(){
+        System.out.println("Method1");
+    }
+
+}
---
+++ src/test/testClass2.java
@@ -0,0 +1,7 @@
+public class testClass2 {
+
+    public void testMethod1(){
+        System.out.println("Test Class 2 - Method1");
+    }
+
+}
